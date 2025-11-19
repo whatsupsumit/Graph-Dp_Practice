d4935ea75006f943c5c3b0e4171f3e1e5920e2a7
Sumit Kumar <115339907+whatsupsumit@users.noreply.github.com>
2025-11-19 19:46:13 +0530

    Time: 19 ms (4.3%), Space: 31.5 MB (7.51%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        map<int, int> mp;
+        unordered_map<int, int> mp;
         for(int x : nums) {
             mp[x]++;
         }
