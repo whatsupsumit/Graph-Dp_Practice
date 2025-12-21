6fb41d91650f792403e8ce4fdee285e9cf13f647
Sumit Kumar <115339907+whatsupsumit@users.noreply.github.com>
2025-12-21 19:43:16 +0530

    Time: 0 ms (100%), Space: 19.6 MB (30.48%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int left = 0;
-        int right = numbers.size()-1;
+        // int left = 0;
+        // int right = numbers.size()-1;
         
-        while(left< right)
-        {
-            int sum = numbers[left] + numbers[right];
-            if(sum==target) return{left+1, right+1};
+        // while(left< right)
+        // {
+        //     int sum = numbers[left] + numbers[right];
+        //     if(sum==target) return{left+1, right+1};
 
-            else if(sum<target)
-            {
-                left++;
+        //     else if(sum<target)
+        //     {
+        //         left++;
+        //     }
+        //     else if(sum>target) right--;
+        // }
+        // return {};
+
+        // using maps
+        map<int,int> mp;
+        for(int i=0; i< numbers.size(); i++)
+        {
+            int diff = target - numbers[i];
+            if (mp.find(diff) != mp.end()) {
+                return {mp[diff] + 1, i + 1};
             }
-            else if(sum>target) right--;
+            mp[numbers[i]] = i;
         }
         return {};
     }
